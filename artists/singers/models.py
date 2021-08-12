fdc7569ec80ea4c0da0033c964b10b17133de6ee
Pinkal-CrossML <88371180+Pinkal-CrossML@users.noreply.github.com>
2021-08-12 12:05:05 +0530

    change in models

--- artists/singers/models.py
+++ artists/singers/models.py
@@ -7,10 +7,20 @@ class Singer(models.Model):
 	create model for Singer 
 	"""
 	singer_name  = models.CharField(max_length=250)
-	singer_id = models.IntegerField()
 
 	def __str__(self):
 		return self.singer_name
+
+class Album(models.Model):
+	"""
+	Model for albums
+    with one to many relation with songs
+	"""
+	album_name = models.CharField(max_length=200)
+	release_date = models.DateTimeField('release date')
+
+	def __str__(self):
+		return self.album_name
 
 class Song(models.Model):
 	"""
@@ -20,22 +30,9 @@ class Song(models.Model):
 	"""
 
 	song_name = models.CharField(max_length=250)
-	song_genre = models.CharField(max_length=200)
-	song_length = models.DurationField()
+	song_length = models.DecimalField(max_digits=4,
+								      decimal_places=2)
 	singer = models.ManyToManyField(Singer)
-
+	album = models.ForeignKey(Album, on_delete=models.CASCADE)
 	def __str__(self):
 		return self.song_name
-
-
-class Album(models.Model):
-	"""
-	Model for albums
-    with one to many relation with songs
-	"""
-	song = models.ForeignKey(Song, on_delete=models.CASCADE)
-	album_name = models.CharField(max_length=200)
-	release_date = models.DateTimeField('release date')
-
-	def __str__(self):
-		return self.album_name
