f19e469e69b491afef5154f5ccc08689edda98dc
pinkal-crossml <pinkal@crossnl.com>
2021-08-18 23:57:59 +0530

    added mysit model

--- artists/singers/models.py
+++ artists/singers/models.py
@@ -6,18 +6,12 @@ class Singer(models.Model):
 	"""
 	create model for Singer 
 	"""
-<<<<<<< HEAD
-	name  = models.CharField(max_length=250)
-=======
 	singer_name  = models.CharField(max_length=250)
->>>>>>> fdc7569ec80ea4c0da0033c964b10b17133de6ee
 
 	def __str__(self):
 		return self.name
 
 class Album(models.Model):
-<<<<<<< HEAD
-=======
 	"""
 	Model for albums
     with one to many relation with songs
@@ -29,33 +23,10 @@ class Album(models.Model):
 		return self.album_name
 
 class Song(models.Model):
->>>>>>> fdc7569ec80ea4c0da0033c964b10b17133de6ee
 	"""
 	Model for albums
     with one to many relation with songs
 	"""
-<<<<<<< HEAD
-	name = models.CharField(max_length=200)
-	release_date = models.DateTimeField('release date')
-
-	def __str__(self):
-		return self.name
-
-class Song(models.Model):
-	"""
-	Song Model for songs
-    Many to many relation with singer
-
-	"""
-
-	name = models.CharField(max_length=250)
-	length = models.DecimalField(max_digits=4,
-								      decimal_places=2)
-	singer = models.ManyToManyField(Singer)
-	album = models.ForeignKey(Album, on_delete=models.CASCADE)
-	def __str__(self):
-		return self.name
-=======
 
 	song_name = models.CharField(max_length=250)
 	song_length = models.DecimalField(max_digits=4,
@@ -64,4 +35,3 @@ class Song(models.Model):
 	album = models.ForeignKey(Album, on_delete=models.CASCADE)
 	def __str__(self):
 		return self.song_name
->>>>>>> fdc7569ec80ea4c0da0033c964b10b17133de6ee
