278884afe85aa2955c77ec20e60d2812c3341bbe
pinkal-crossml <pinkal@crossnl.com>
2021-09-01 17:14:29 +0530

    final commit

--- Document_manager/Doc_app/views.py
+++ Document_manager/Doc_app/views.py
@@ -7,7 +7,7 @@ from . forms import DocumentForm
 from django.contrib.auth.decorators import login_required
 from django.core.exceptions import ValidationError
 from django.utils import timezone
-
+import datetime
 
         
 @login_required
@@ -64,10 +64,42 @@ def login(request):
 
 def upload_page(request):
     form=DocumentForm
-    return render(request, 'Doc_app/upload.html', {
-        'form': form
-    })
-    
+    doc=Document.objects.all()
+    if request.user.is_authenticated:
+            today = datetime.date.today()
+            print(today)
+            current_month = datetime.date.today().month
+            current_year = datetime.date.today().year
+            user=request.user.id
+            user_obj=User.objects.get(pk=user)
+            print(user_obj)
+            pdf_list=Document.objects.all().filter(user=user_obj.pk)
+            print(pdf_list)
+            if request.method == 'GET':
+                report_type = request.GET.get('report_type')
+                if report_type == 'sort_by_name':
+                    user_data = Document.objects.filter(user=User.objects.get(username=request.user.username)).order_by('description')
+                    return render(request, 'Doc_app/upload.html',{'form':form,'context':user_data})
+                elif report_type=='current_month':
+                    user_data = Document.objects.filter(user=User.objects.get(username=request.user.username)).filter(uploaded_at__month=current_month)
+                    return render(request, 'Doc_app/upload.html', {'form':form,'context': user_data})
+                elif report_type=='current_year':
+                    user_data = Document.objects.filter(user=User.objects.get(username=request.user.username)).filter(uploaded_at__year=current_year)
+                    return render(request, 'Doc_app/upload.html', {'form':form,'context': user_data})
+                elif report_type=='doc_range':
+                    start_date = request.POST['startdate']
+                    end_date = request.POST['enddate']
+                    print(start_date,end_date)
+                    user_data = Document.objects.filter(user=User.objects.get(username=request.user.username)).filter(uploaded_at__range=[start_date, end_date])
+                    print(user_data)
+                    return render(request, 'Doc_app/upload.html', {'form':form,'context': user_data})
+            elif request.method=='POST':
+            # breakpoint()
+                start_date = request.POST['startdate']
+                end_date = request.POST['enddate']
+                user_data = pdf_list.filter(user=request.user, uploaded_at__range=[start_date,end_date])
+                return render(request, 'Doc_app/upload.html', {'form':form,'context': user_data})
+            return render(request, 'Doc_app/upload.html',{'form':form,'context':pdf_list})
 
    
 
